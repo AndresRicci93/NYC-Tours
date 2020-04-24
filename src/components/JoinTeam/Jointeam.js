d1e73281e8c3bc3bccbd8ae3c5e5b76514f76ec4
unknown <andresricci93@gmail.com>
2020-04-25 00:47:55 +0200

    first commit

---
+++ src/components/JoinTeam/Jointeam.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Jointeam extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Join the team!</h1>
+            </div>
+        )
+    }
+}
